17af3420a15c139e2b0f9404447f9be30c75b66a
Tim <timothyjames.short@gmail.com>
2017-09-20 11:42:09 -0400

    Submitted final weather trends project for review

--- 01_intro/project_steps.sql
+++ 01_intro/project_steps.sql
@@ -18,9 +18,32 @@ SELECT *
 FROM global_data
 
 #4. Add 7-day and 14-day moving averages in Excel, export to CSV
+average(B2:B8)
+average(B2:B15)
+
 
 #5. Import data into RStudio
     #Add library(ggplot)
-ggplot(global_temp_data, aes(x=year)) + geom_point(aes(y=avg_temp), size=.5, color="gray") + geom_line(aes(y=moving_avg_14), color="blue", size=1)
+    
+ggplot(columbus_temp_data, aes(x=year)) + ylim(12,16) + geom_line(aes(y=moving_avg_14), color="red", size=1) + geom_point(aes(y=avg_temp), size=.5, color="gray")
 
-ggplot(columbus_temp_data, aes(x=year)) + geom_point(aes(y=avg_temp), size=.5, color="gray") + geom_line(aes(y=moving_avg_14), color="red", size=1)
+ggplot(global_temp_data, aes(x=year)) + ylim(6,10) + geom_line(aes(y=moving_avg_14), color="blue", size=1) + geom_point(aes(y=avg_temp), size=.5, color="gray")
+
+#6. Analyze
+
+#Compare City vs Global temperatures by looking at the average and the most recent 14-year moving average
+global_avg = median(global_temp_data$avg_temp[global_temp_data$year>1750], na.rm = TRUE)
+> 8.37 
+columbus_avg = median(columbus_temp_data$avg_temp[columbus_temp_data$year>1750], na.rm = TRUE)
+> 14.06
+
+tail(global_temp_data$moving_avg_14,1)
+> 9.58
+tail(columbus_temp_data$moving_avg_14,1)
+> 15.03
+
+#Find the average change from year 1763 to 2013
+(global_temp_data$moving_avg_14[264] - global_temp_data$moving_avg_14[14]) / (264-14)
+> .00596
+(columbus_temp_data$moving_avg_14[271] - columbus_temp_data$moving_avg_14[21]) / (271-21)
+> .0074
